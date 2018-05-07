4d55fe24c71246e19c52e4e1145b7ff1d1e270f7
danielemsilva <daniele.silva@ccc.ufcg.edu.br>
2018-05-07 11:57:45 -0300

    Implementation of bubble sort and insertion sort

---
+++ Bubble_sort.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String[] input = sc.nextLine().split(" ");
+    int[] integers = new int[input.length];
+    for (int i = 0; i < input.length; i++) {
+      integers[i] = Integer.valueOf(input[i]);
+    }
+    sort(integers);
+  }
+
+  private static void sort(int[] array) {
+		boolean changed = false;
+		do {
+			changed = false;
+			for (int i = 0; i < array.length - 1; i++) {
+				if (array[i] > array[i + 1]) {
+				  int aux = array[i];
+				  array[i] = array[i + 1];
+				  array[i + 1] = aux;
+					changed = true;
+	      }
+	    }
+	    if (changed) {
+  	    printArray(array);
+  	  }
+		} while(changed);
+
+    if (array.length == 1) {
+      printArray(array);
+    }
+  }
+
+  private static void printArray(int[] array) {
+    for (int i = 0; i < array.length; i++) {
+      System.out.print(array[i]);
+      if (i + 1 != array.length) {
+        System.out.print(" ");
+      }
+    }
+    System.out.println();
+  }
+}
---
+++ Insertion_recursivo.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String[] input = sc.nextLine().split(" ");
+    int[] integers = new int[input.length];
+    for (int i = 0; i < input.length; i++) {
+      integers[i] = Integer.valueOf(input[i]);
+    }
+    sort(integers, 0, integers.length - 1);
+  }
+
+  private static void sort(int[] array, int leftIndex, int rightIndex) {
+		for (int i = leftIndex; i > 0; i--) {
+			if (array[i] < array[i - 1]) {
+			  int aux = array[i];
+			  array[i] = array[i - 1];
+			  array[i - 1] = aux;
+			}
+		}
+		if (leftIndex != 0) {
+		  printArray(array);
+		}
+    if (leftIndex < rightIndex) {
+      leftIndex++;
+      sort(array, leftIndex, rightIndex);
+    }
+  }
+
+  private static void printArray(int[] array) {
+    System.out.println(Arrays.toString(array));
+  }
+}
